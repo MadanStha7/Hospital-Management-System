4707315d6e960371ee60f60ff87131b823f7b48e
leapfrog <leapfrog@pop-os.localdomain>
2022-02-26 10:04:37 +0545

    admin layout design completed in home page section

--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("",include('user.urls'))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("user.urls"))]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
