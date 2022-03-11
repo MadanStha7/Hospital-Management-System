8adaaa9b15ec2797792d8cba2eca31cb76ebaedc
leapfrog <leapfrog@pop-os.localdomain>
2022-03-11 09:11:25 +0545

    appoinement ui completed in opatient dashboard

--- user/models.py
+++ user/models.py
@@ -87,7 +87,7 @@ class Doctor(models.Model):
     shift = models.CharField(choices=SHIFT, max_length=2, null=True, blank=True)
     location = models.CharField(max_length=100)
     dob = models.DateField(("Date of birth"), null=True)
-    image = models.ImageField(upload_to="patient")
+    image = models.ImageField(upload_to="doctor",null=True,blank=True)
     experience = models.CharField(max_length=100, null=True)
     specialist = models.CharField(max_length=100, null=True)
     clinic = models.CharField(max_length=100, null=True, blank=True)
