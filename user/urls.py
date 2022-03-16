ca02d914208505fb9df4c6e772277436d926e0ed
MadanStha7 <madanstha.124@gmail.com>
2022-03-16 13:48:25 +0545

    doctor profile view

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from user.routes.admin_urls import urlpatterns as admin_urls
 from user.routes.patient_urls import urlpatterns as user_urls
+from user.routes.doctor_urls import urlpatterns as doctor_urls
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns = admin_urls + user_urls
+
+urlpatterns = admin_urls + user_urls + doctor_urls
