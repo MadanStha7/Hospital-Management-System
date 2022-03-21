b2b57ef910628235b8291a7593475e3273876794
viper <leapfrog@pop-os.localdomain>
2022-03-21 16:48:15 +0545

    doctor,patient,hsopital add

--- user/routes/admin_urls.py
+++ user/routes/admin_urls.py
@@ -1,4 +1,49 @@
 from django.urls import path
-from user.viewsets.admin_views import AdminView
+from user.viewsets.admin_views import (
+    AdminView,
+    DoctorListView,
+    PatientListView,
+    HospitalListView,
+    DoctorDetailView,
+    DoctorDeleteView,
+    PatientDetailView,
+    patientDeleteView,
+    HospitalDetailView,
+    HospitalDeleteView
+)
 
-urlpatterns = [path("admins/", AdminView.as_view(), name="admin-dashboard")]
+urlpatterns = [
+    path("admin-dashboard/", AdminView.as_view(), name="admin-dashboard"),
+    # doctor
+    path("doctor-list/", DoctorListView.as_view(), name="doctor-list"),
+    path(
+        "doctor-detail/<int:pk>", DoctorDetailView.as_view(), name="admin-doctor-detail"
+    ),
+    path(
+        "doctor-delete/<int:pk>", DoctorDeleteView.as_view(), name="admin-doctor-delete"
+    ),
+    # patient
+    path("patient-list/", PatientListView.as_view(), name="patient-list"),
+    path(
+        "patient-detail/<int:pk>",
+        PatientDetailView.as_view(),
+        name="admin-patient-detail",
+    ),
+    path(
+        "patient-delete/<int:pk>",
+        patientDeleteView.as_view(),
+        name="admin-patient-delete",
+    ),
+    #hospitals
+    path("hospital-list/", HospitalListView.as_view(), name="hospital-list"),
+    path(
+        "hospital-detail/<int:pk>",
+        HospitalDetailView.as_view(),
+        name="admin-hospital-detail",
+    ),
+    path(
+        "hospital-delete/<int:pk>",
+        HospitalDeleteView.as_view(),
+        name="admin-hospital-delete",
+    ),
+]
--- user/viewsets/admin_views.py
+++ user/viewsets/admin_views.py
@@ -1,6 +1,80 @@
+import io
+from django.forms.models import model_to_dict
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import DeleteView
+from user.models import Doctor, Patient, Appointment, Hospital
+from django.urls import reverse_lazy
 
 
 class AdminView(TemplateView):
     template_name = "admin/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["doctors"] = Doctor.objects.all().count()
+        context["patients"] = Patient.objects.all().count()
+        context["appointments"] = Appointment.objects.all().count()
+        context["hospitals"] = Hospital.objects.all().count()
+        return context
+
+
+class DoctorListView(TemplateView):
+    template_name = "admin/doctor-list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["doctors"] = Doctor.objects.all()
+        return context
+
+
+class PatientListView(TemplateView):
+    template_name = "admin/patient-list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["patients"] = Patient.objects.all()
+        return context
+
+
+class HospitalListView(TemplateView):
+    template_name = "admin/hospital-list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["hospitals"] = Hospital.objects.all()
+        return context
+
+
+class DoctorDetailView(DetailView):
+    template_name = "admin/doctor-detail.html"
+    model = Doctor
+    context_object_name = "doctor"
+
+
+class DoctorDeleteView(DeleteView):
+    model = Doctor
+    success_url = reverse_lazy("doctor-list")
+
+
+class PatientDetailView(DetailView):
+    template_name = "admin/patient-detail.html"
+    model = Patient
+    context_object_name = "patient"
+
+
+class patientDeleteView(DeleteView):
+    model = Patient
+    success_url = reverse_lazy("patient-list")
+
+
+class HospitalDetailView(DetailView):
+    template_name = "admin/hospital-detail.html"
+    model = Hospital
+    context_object_name = "hospital"
+
+
+class HospitalDeleteView(DeleteView):
+    model = Hospital
+    success_url = reverse_lazy("hospital-list")
