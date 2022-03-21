cef7c90571fdae499f98c67406de1e3d4a89fefe
MadanStha7 <madanstha.124@gmail.com>
2022-03-21 15:49:21 +0545

    doctor password change, logout, my patients and prescriptions done

--- user/routes/doctor_urls.py
+++ user/routes/doctor_urls.py
@@ -3,7 +3,11 @@ from user.viewsets.doctor_views import (
     DoctorDashboard,
     DoctorProfile,
     DoctorProfileUpdateView,
+    PrescriptionView,
     accept_appointment,
+    doctor_changepassword,
+    MyPatientView,
+    PrescriptionCreateView
 )
 
 urlpatterns = [
@@ -15,4 +19,13 @@ urlpatterns = [
         name="doctor-update",
     ),
     path("accept/appointment/<int:pk>/", accept_appointment, name="accept-appointment"),
+    path(
+        "doctor-change-password",
+        doctor_changepassword,
+        name="doctor-change-password",
+    ),
+    path("my-patient/", MyPatientView.as_view(), name="my-patient"),
+    path("prescription-list/", PrescriptionView.as_view(), name="prescription-list"),
+    path("prescription-create/", PrescriptionCreateView.as_view(), name="prescription-create"),
+    
 ]
--- user/viewsets/doctor_views.py
+++ user/viewsets/doctor_views.py
@@ -1,10 +1,12 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, UpdateView
-from user.models import Doctor, Appointment
-from user.forms import DoctorProfileForm
-from datetime import date, datetime
+from django.views.generic import TemplateView, UpdateView, CreateView
+from user.models import Doctor, Appointment, Prescription
+from user.forms import DoctorProfileForm, PrescriptionCreateForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from datetime import date
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 class DoctorDashboard(TemplateView):
@@ -12,18 +14,25 @@ class DoctorDashboard(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        logged_user = self.request.user
         try:
-            user = Doctor.objects.get(user=self.request.user)
+            user = Doctor.objects.get(user=logged_user)
         except Doctor.DoesNotExist:
             user = None
 
         context["user"] = user
-        context["appointment_list"] = Appointment.objects.all()
-        context["total_appointment"] = Appointment.objects.all().count()
-        context["pending_appointment"] = Appointment.objects.filter(status="P").count()
+        context["appointment_list"] = Appointment.objects.filter(
+            doctor__user=logged_user
+        )
+        context["total_appointment"] = Appointment.objects.filter(
+            doctor__user=logged_user
+        ).count()
+        context["pending_appointment"] = Appointment.objects.filter(
+            doctor__user=logged_user, status="P"
+        ).count()
         today = date.today()
         context["today_appointment"] = Appointment.objects.filter(
-            app_date=today
+            doctor__user=logged_user, app_date=today
         ).count()
 
         return context
@@ -63,3 +72,52 @@ def accept_appointment(request, pk):
         messages.success(request, "Appointment Accepted")
         return redirect("doctor-dashboard")
 
+
+def doctor_changepassword(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, "Your password was successfully updated!")
+            return redirect("login")
+        else:
+            messages.error(request, "Please correct the error below.")
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, "doctor/change-password.html", {"form": form})
+
+
+class MyPatientView(TemplateView):
+    template_name = "doctor/my-patient.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        logged_user = self.request.user
+        context["appointment_list"] = Appointment.objects.filter(doctor__user=logged_user, status="A")
+        return context
+
+
+class PrescriptionView(TemplateView):
+    template_name = "doctor/prescription.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        logged_user = self.request.user
+        context["prescription_list"] = Prescription.objects.filter(appointement__doctor__user=logged_user)
+        return context
+
+class PrescriptionCreateView(CreateView):
+    template_name = 'doctor/prescriptioncreate.html'
+    form_class = PrescriptionCreateForm
+    success_url = reverse_lazy('prescription-list')
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        doctor = Doctor.objects.get(user=self.request.user)
+        form.instance.doctor = doctor
+        return super().form_valid(form)
