1829faa6868991e7071e00efcd71d18b27b0e034
viper <thaparam78666@gmail.com>
2022-04-16 11:13:00 +0545

    [fix] image in title

--- project/settings.py
+++ project/settings.py
@@ -109,13 +109,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR), "static_cdn")
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static_cdn"),
+    os.path.join(BASE_DIR, "static"),
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR), "media_cdn")
 MEDIAFILES_DIRS = [
     os.path.join(BASE_DIR, "media_cdn"),
 ]
